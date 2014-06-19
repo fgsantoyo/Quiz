4e0e769451b970544172d44bb516b771b286e620
fgsantoyo <fgsantoyo@gmail.com>
2014-06-18 21:30:28 -0400

    Created project and added the FGQuizViewController with XIB UI file with UI elements connected to the view Controller

---
+++ Quiz/FGQuizViewController.h
@@ -0,0 +1,17 @@
+//
+//  FGQuizViewController.h
+//  Quiz
+//
+//  Created by Fernando Garcia, Jr on 6/18/14.
+//  Copyright (c) 2014 Fernando Garcia, Jr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FGQuizViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *questionLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *answerLabel;
+
+@end
---
+++ Quiz/FGQuizViewController.m
@@ -0,0 +1,46 @@
+//
+//  FGQuizViewController.m
+//  Quiz
+//
+//  Created by Fernando Garcia, Jr on 6/18/14.
+//  Copyright (c) 2014 Fernando Garcia, Jr. All rights reserved.
+//
+
+#import "FGQuizViewController.h"
+
+@interface FGQuizViewController ()
+
+@end
+
+@implementation FGQuizViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)showQuestion:(id)sender {
+    
+}
+
+- (IBAction)showAnswer:(id)sender {
+    
+}
+
+@end
