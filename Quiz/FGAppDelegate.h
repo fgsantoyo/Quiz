32a1a3b250b026cabda7bd375ff5009dd46027a3
fgsantoyo <fgsantoyo@gmail.com>
2014-06-18 20:54:58 -0400

    Initial Commit

---
+++ Quiz/FGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FGAppDelegate.h
+//  Quiz
+//
+//  Created by Fernando Garcia, Jr on 6/18/14.
+//  Copyright (c) 2014 Fernando Garcia, Jr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Quiz/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Quiz
+//
+//  Created by Fernando Garcia, Jr on 6/18/14.
+//  Copyright (c) 2014 Fernando Garcia, Jr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FGAppDelegate class]));
+    }
+}
